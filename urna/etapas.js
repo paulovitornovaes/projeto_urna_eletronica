4580e7cf4dd9af7f19d479b98edacfbda51bb146
paulovitornovaes <paulonovaes@id.uff.br>
2021-06-22 22:55:43 -0300

    Initial commit

---
+++ urna/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'Presidente',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                name: "Dilma Roussef",
+                partido: 'PT',
+                vice: 'Temer',
+                fotos: [
+                    { url: 'dilma.jpg', legenda: 'Presidente'},
+                    {url: 'temer.jpg', legenda: 'Vice-presidente', small: true}
+                ]
+            },
+            {
+                numero: '17',
+                name: "Jair Messias Bolsonaro",
+                partido: "Nenhum",
+                fotos:[
+                    {url:'bolsonaro.jpeg', legenda: 'Presidente'},
+                    {url: 'mourao.jpg', legenda: 'Vice-presidente', small: true}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'Governador',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '700',
+                name: "Witzel",
+                partido: "PSC",
+                fotos:[
+                    {url:'witzel.jpg', legenda: 'Governador'}
+                ]
+            },
+            {
+                numero: '100',
+                name: "Amoedo",
+                partido: "NOVO",
+                fotos:[
+                    {url: 'amoedo.jpg', legenda: 'Governador'}
+                ]
+            }
+        ]
+
+    }
+
+]
